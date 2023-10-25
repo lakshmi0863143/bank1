d727cfa9170cd0b89a5b3b2b30ce037036bdd69f
sowmyas1117@gmail.com <sowmyas1117@gmail.com>
2023-10-25 06:42:56 +0000

    Edited settings and landingmodule.borrower_page_landing_form.view_profile

--- client_code/landingmodule/borrower_page_landing_form/view_profile/__init__.py
+++ client_code/landingmodule/borrower_page_landing_form/view_profile/__init__.py
@@ -14,6 +14,32 @@ class view_profile(view_profileTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    
+    self.user_id = []
+    self.name = []
+    self.password = []
+    self.mobile_no = []
+    self.mail_id = []
+    self.pincode = []
+    
+
+    data = tables.app_tables.users.search()
+    for row in data:
+      self.user_id.append(row['user_id'])
+      self.name.append(row['name'])
+      self.password.append(row['passward'])
+      self.mobile_no.append(row['mobile_no'])
+      self.mail_id.append(row['mail_id'])
+      self.pincode.append(row['pincode'])
+    
+    self.label_3.text = self.user_id[-1]
+    self.label_5.text = self.name[-1]
+    self.label_7.text = self.password[-1]
+    self.label_9.text = self.mobile_no[-1]
+    self.label_11.text = self.mail_id[-1]
+    self.label_13.text = self.pincode[-1]
+    print(self.pincode)
+
 
   def button_2_click(self, **event_args):
     """This method is called when the button is clicked"""

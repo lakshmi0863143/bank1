9c3f21d5bbfae44c903cc1b06ab86346c1cb97d6
sowmyas1117@gmail.com <sowmyas1117@gmail.com>
2023-10-26 09:40:54 +0000

    Edited landingmodule.borrower_page_landing_form.new_loan

--- client_code/landingmodule/borrower_page_landing_form/new_loan/__init__.py
+++ client_code/landingmodule/borrower_page_landing_form/new_loan/__init__.py
@@ -14,3 +14,59 @@ class new_loan(new_loanTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('landingmodule.borrower_page_landing_form')
+
+  def radio_button_1_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    ri = self.radio_button_1.selected
+
+  def radio_button_2_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    nri = self.radio_button_2.selected
+
+  def radio_button_3_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    oci = self.radio_button_3.selected
+
+  def text_box_1_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    firstname = self.text_box_1.text
+
+  def text_box_2_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    middlename = self.text_box_2.text
+
+  def text_box_3_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    lastname = self.text_box_3.text
+
+  def date_picker_1_change(self, **event_args):
+    """This method is called when the selected date changes"""
+    birthdate = self.date_picker_1.date
+
+  def text_box_4_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    mobileno = self.text_box_4.text
+
+  def text_box_5_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    location = self.text_box_5.text
+
+  def drop_down_1_change(self, **event_args):
+    """This method is called when an item is selected"""
+    min_amount = self.drop_down_1.selected_value
+
+
+
+
+
+
+
+    
+
+
+
+
